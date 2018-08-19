2140f178835060609bf35ec2da3cc4257947a8d2
ikuyako <yuki416oka@gmai.com>
2018-08-20 01:07:45 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -54,7 +54,7 @@ getElementById('assessment');
     
     const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
-        '{userName}のいいところはまなざしです。{userName}'に見つめられた人は、気になって仕方がないでしょう。',
+        '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
         '{userName}のいいところは情熱です。{userName}の情熱の周りの人は感化されます。',
         '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
         '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
@@ -62,7 +62,7 @@ getElementById('assessment');
         '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
         '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
         '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-        '{userName}のいいところは思いやりです。{userName}'に気をかけてもらった多くの人が感謝しています。',
+        '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
         '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
         '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
         '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
